05566c6bd41dd6329991630d690db2f502958917
stephen cleary <steve0cleary@gmail.com>
2023-04-05 19:56:27 +0000

    changes to main.tf

--- main.tf
+++ main.tf
@@ -318,7 +318,8 @@ resource "aws_instance" "web_server_2" {
 module "server" {
   source          = "./modules/server"
   ami             = data.aws_ami.ubuntu.id
-  size            = "t2.micro"
+  size            = "t2.micro
+  "
   subnet_id       = aws_subnet.public_subnets["public_subnet_3"].id
   security_groups = [aws_security_group.vpc-ping.id, aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
 }
